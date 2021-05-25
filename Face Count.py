024d683fa7fb23476ed387a3ab98a33638692a9f
Alfredo <76714792+aalejo92@users.noreply.github.com>
2021-05-24 20:29:19 -0400

    Update Face Count.py
    05-24-21 - Added face count print for both images

--- Face Count.py
+++ Face Count.py
@@ -1,7 +1,10 @@
-
 import face_recognition
 
-image = face_recognition.load_image_file('./Images/group1.jpg')
-face_locations = face_recognition.face_locations(image)
+image1 = face_recognition.load_image_file('./Images/group1.jpg')
+image2 = face_recognition.load_image_file('./Images/group2.jpg')
+face_location_1 = face_recognition.face_locations(image1)
+face_location_2 = face_recognition.face_locations(image2)
 
-print(f'There are {len(face_locations)} people in this image')
+print(
+    f'There are {len(face_location_1)} people in the first image '
+    f'and {len(face_location_2)} people in the second image')
